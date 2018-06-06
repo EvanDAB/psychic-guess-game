f40e3c1eb843f6a76c91898073cf7668bbbfcc9d
Evan Carr <37350180+EvanDAB@users.noreply.github.com>
2018-06-05 18:23:30 -0700

    done

--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -3,25 +3,23 @@ var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
                 'x', 'y', 'z'];
 
 var rand = alphabet[Math.floor(Math.random() * alphabet.length)];
- 
+
 var wins = 0;
 var losses = 0;
-var guessLeft = 9;    
+var guessLeft = 9; 
 
-
-function guess (event) {  
-    var x = event.key;
+function guess (event) {
+    var key = event.key;
 
     var guessList= document.querySelector('#guessList');
+    guessList.innerText = guessList.innerText + ` ${key}, `;
 
-    guessList.innerText = guessList.innerText + ` ${x}, `;
-
-    if (x == rand) {
+    if (key == rand) {
         wins++;
         guessLeft--;
         document.getElementById("wins").innerHTML = wins;
         document.getElementById("guessLeft").innerHTML = guessLeft;
-    } else { 
+    } else {
         losses++;
         guessLeft--;
         document.getElementById("losses").innerHTML = losses;
@@ -30,14 +28,9 @@ function guess (event) {
 
     if (guessLeft == 0) {
         guessLeft = 9;
-        document.getElementById("guessLeft").innerHTML = guessLeft;
         guessList.innerText = '';
         rand = alphabet[Math.floor(Math.random() * alphabet.length)];
     }
+}
 
-    }
-
-
-document.onkeypress = guess;
-
-
+document.onkeyup = guess;
